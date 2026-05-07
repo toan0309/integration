f53db98b02a1034ed84237576047aff35b010708
toan0309 <46134381+toan0309@users.noreply.github.com>
2026-05-08 01:51:03 +0700

    setup base frontend files

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import "./Dashboard.scss";
+
+function Dashboard() {
+  return (
+    <div className="dashboard-page">
+      <h1>Dashboard</h1>
+      <p>HR Payroll Integration Dashboard</p>
+    </div>
+  );
+}
+
+export default Dashboard;
